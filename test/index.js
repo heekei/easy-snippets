ddf35b38bc1f400eda5248b2e0d274284b067a99
Heekei <zxq0610@qq.com>
2019-06-20 19:37:35 +0800

    mocha

--- test/index.js
+++
@@ -1,2 +0,0 @@
-// import lib from '../dist/easy-snippets';
-const { compareVersion } = require('../dist/easy-snippets');
---
+++ test/units/compareVersion.js
@@ -0,0 +1,20 @@
+const { describe, it } = require('mocha');
+const assert = require('assert');
+const { compareVersion } = require('../../dist/easy-snippets');
+describe('compareVersion',function(){
+  describe('("1.4.3","1.3.2")', function () {
+    it('should return -1 when v1>v2', function () {
+      assert.equal(compareVersion('1.4.3','1.3.2'), 1);
+    });
+  });
+  describe('("1.2.3","1.3.2")', function () {
+    it('should return -1 when v1<v2', function () {
+      assert.equal(compareVersion('1.2.3','1.3.2'), -1);
+    });
+  });
+  describe('("1.2.3","1.2.3")', function () {
+    it('should return -1 when v1=v2', function () {
+      assert.equal(compareVersion('1.2.3','1.2.3'), 0);
+    });
+  });
+});
