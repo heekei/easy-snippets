c33c62a8394a2bccc8c5090ba7ed1b3fc1394e91
Heekei <zxq0610@qq.com>
2019-06-30 22:57:15 +0800

    test src

--- test/units/compareVersion.test.js
+++ test/units/compareVersion.test.js
@@ -1,6 +1,6 @@
 const { describe, it } = require('mocha');
 const assert = require('assert');
-const { compareVersion } = require('../../dist/easy-snippets');
+const { compareVersion } = require('../../src/index');
 describe('compareVersion',function(){
   describe('("1.4.3","1.3.2")', function () {
     it('should return -1 when v1>v2', function () {
